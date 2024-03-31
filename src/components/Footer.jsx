4275dbc4f2d48c9dd81d2c65273409931bc5b110
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-03-31 10:32:24 -0400

    components done. full id and classname revamp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,16 @@ function Footer() {
             <h3 id="footer__subtitle">Contact Us:</h3>
             <nav id="footer__nav">
                 <div>
-                    <img id="footer__nav__email-icon" src="" alt="Email Icon"/>
+                    <img id="footer__nav__email-icon" src="./assets/email.png" alt="Email Icon"/>
                     <a href="mailto:abdelrahman.s2003@icloud.com" target="_blank">Email</a>
+                </div>
+                <div>
+                    <img id="footer__nav__tel-icon" src="./assets/telephone.png" alt="Phone Icon"/>
+                    <a href="tel:631-485-7394" target="_blank">Call</a>
+                </div>
+                <div>
+                    <img id="footer__nav__linkedin-icon" src="./assets/linkedin.png" alt="LinkedIn Icon"/>
+                    <a href="https://www.linkedin.com/in/abdelsayedahmed/" target="_blank">LinkedIn</a>
                 </div>
             </nav>
         </div>
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,9 @@
 function Popup() {
     return (
         <div id="popup">
-            <div id="popup-bin">
-                <p>Are your sure you want to remove?</p>
-                <button id="popup__cancel-btn">Cancel</button>
-                <button id="popup__remove-btn">Remove</button>
-            </div>
+            <p>Are your sure you want to remove?</p>
+            <button id="popup__cancel-btn">Cancel</button>
+            <button id="popup__remove-btn">Remove</button>
         </div>
     )
 }
